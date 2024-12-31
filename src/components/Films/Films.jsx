5cd7c5941d19e477b7620f94714807317288a1e0
Mohamed Ndoye <ndoye.mohamed95@yahoo.fr>
2024-12-31 13:31:17 +0100

    modif page films

--- src/components/Films/Films.jsx
+++ src/components/Films/Films.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./Films.module.css";
-import film1 from "../../assets/img/fims/man_of_steel.webp";
-import film2 from "../../assets/img/fims/superman_batman_ennemis_publics.jpg";
-import film3 from "../../assets/img/fims/batman_v_superman_l_aube_de_la_justice.jpg";
-import film4 from "../../assets/img/fims/superman_returns.jpg";
-import film5 from "../../assets/img/fims/superman_ii.jpg";
-import film6 from "../../assets/img/fims/superman_iii.jpg";
-import film7 from "../../assets/img/fims/zack_snyder_s_justice_league.webp";
-import film9 from "../../assets/img/fims/justice_league.jpg";
-import film10 from "../../assets/img/fims/superman_ii_the_richard_donner_cut.webp";
-import film11 from "../../assets/img/fims/superman_iv_le_face_a_face.webp";
-import film12 from "../../assets/img/fims/superman_le_crepuscule_d_un_dieu.webp";
-import film13 from "../../assets/img/fims/superman_batman_apocalypse.jpg";
-import film14 from "../../assets/img/fims/superman_batman_ennemis_publics.jpg";
+import film1 from "../../assets/img/films/man_of_steel.webp";
+import film2 from "../../assets/img/films/superman_batman_ennemis_publics.jpg";
+import film3 from "../../assets/img/films/batman_v_superman_l_aube_de_la_justice.jpg";
+import film4 from "../../assets/img/films/superman_returns.jpg";
+import film5 from "../../assets/img/films/superman_ii.jpg";
+import film6 from "../../assets/img/films/superman_iii.jpg";
+import film7 from "../../assets/img/films/zack_snyder_s_justice_league.webp";
+import superman1978 from "../../assets/img/films/superman.jpg";
+import film9 from "../../assets/img/films/justice_league.jpg";
+import film10 from "../../assets/img/films/superman_ii_the_richard_donner_cut.webp";
+import film11 from "../../assets/img/films/superman_iv_le_face_a_face.webp";
+import film12 from "../../assets/img/films/superman_le_crepuscule_d_un_dieu.webp";
+import film13 from "../../assets/img/films/superman_batman_apocalypse.jpg";
+import film14 from "../../assets/img/films/superman_batman_ennemis_publics.jpg";
+import { Link } from "react-router-dom";
 
 function Films() {
   const filmsListe = [
@@ -25,79 +27,80 @@ function Films() {
     {
       id: 2,
       imageUrl: film2,
-      titre: "1. Man of Steel (2013)",
+      titre: "2. Man of Steel (2013)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 3,
       imageUrl: film3,
-      titre: "1. Man of Steel (2013)",
+      titre: "3.Batman v Superman : L'Aube de la Justice (2016)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 4,
       imageUrl: film4,
-      titre: "1. Man of Steel (2013)",
+      titre: "4. Superman Returns (2006)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 5,
       imageUrl: film5,
-      titre: "1. Man of Steel (2013)",
+      titre: "5. Superman II (1980)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 6,
       imageUrl: film6,
-      titre: "1. Man of Steel (2013)",
+      titre: "6. Superman III (1983)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 7,
       imageUrl: film7,
-      titre: "1. Man of Steel (2013)",
+      titre: "7.Zack Snyder's Justice League (2021)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
-    //   {
-    //     id: 8,
-    //     imageUrl:film8,
-    //     titre: "1. Man of Steel (2013)",
-    //     paragraphe: "<p> Film de Zack Snyder </p>",
-    //   },
+    {
+      id: 8,
+      imageUrl: superman1978,
+      titre: "8.Superman (1978)",
+      paragraphe: "<p> Film de Zack Snyder </p>",
+    },
     {
       id: 9,
       imageUrl: film9,
-      titre: "1. Man of Steel (2013)",
+      titre: "9. Justice League (2017)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 10,
       imageUrl: film10,
-      titre: "1. Man of Steel (2013)",
+      titre: `10. Superman IV - Le Face à face (1987)`,
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 11,
       imageUrl: film11,
-      titre: "1. Man of Steel (2013)",
+      titre: `
+11. Superman : Le Crépuscule dun dieu (2007)`,
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 12,
       imageUrl: film12,
-      titre: "1. Man of Steel (2013)",
+      titre: `12. Superman / Batman : Apocalypse (2010)`,
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 13,
       imageUrl: film13,
-      titre: "1. Man of Steel (2013)",
+      titre: "13. Superman / Batman : Apocalypse (2010)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
     {
       id: 14,
       imageUrl: film14,
-      titre: "1. Man of Steel (2013)",
+      titre: " 14.Superman / Batman : Ennemis publics (2010)",
       paragraphe: "<p> Film de Zack Snyder </p>",
     },
   ];
@@ -117,7 +120,7 @@ function Films() {
         <div className={styles.supermanCard}>
           {filmsListe.map((filmsListes) => (
             <div
-              className="m-2 d-flex align-items-center justify-content-center"
+              className={`m-2 d-flex align-items-center justify-content-center  ${styles.blocImgFilms}`}
               key={filmsListes.id}
               // style={{ width: "150px", height: "200px" }}
             >
@@ -127,7 +130,9 @@ function Films() {
                 alt=""
                 // style={{ width: "100%", height: "100%", objectFit: "cover" }}
               />
-              <h5 className="mt-4">{filmsListes.titre}</h5>
+              <Link to="/">
+                <h5 className="mt-4  ">{filmsListes.titre}</h5>
+              </Link>
             </div>
           ))}
         </div>
